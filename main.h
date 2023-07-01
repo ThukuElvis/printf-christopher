8314a05014fe34cc765a3fad589c1b8126aead2b
PRODI3GY <mwaterichristopher@gmail.com>
2023-07-01 03:10:38 +0300

    printf1

---
+++ main.h
@@ -0,0 +1,38 @@
+#ifndef MY_PRINTF
+#define MY_PRINTF
+
+
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <stdarg.h>
+
+#include <stdint.h>
+
+
+
+
+
+struct convert
+
+{
+
+	char *sym;
+
+	int (*f)(va_list);
+
+};
+
+typedef struct convert conver_t;
+
+
+
+
+
+int _putchar(char c);
+
+int _printf(const char *format, ...);
+
+#endif
