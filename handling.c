4bc682c00a9c4c50d4efafed1bec5d92dcff4b50
PRODI3GY <mwaterichristopher@gmail.com>
2023-07-01 07:02:47 +0300

    Finished

--- handling.c
+++ handling.c
@@ -1,215 +1,50 @@
 #include "main.h"
 /**
- * printChar - Print single character
- * @arguments:Arguments
- *
- * Return: number of characters printed
+ * handle_print - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: ind.
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: 1 or 2;
  */
-
-
-
-
-
-int print_number(va_list args)
-
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
 {
+	int i, unknow_len = 0, printed_chars = -1;
+	fmt_t fmt_types[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'i', print_int}, {'d', print_int}, {'b', print_binary},
+		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+	};
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
+		if (fmt[*ind] == fmt_types[i].fmt)
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
-	int n;
-
-	int check;
-
-	int len;
-
-	unsigned int num;
-
-
-
-	n  = va_arg(args, int);
-
-	check = 1;
-
-	len = 0;
-
-
-
-	if (n < 0)
-
+	if (fmt_types[i].fmt == '\0')
 	{
-
-		len += _putchar('-');
-
-		num = n * -1;
-
+		if (fmt[*ind] == '\0')
+			return (-1);
+		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+		else if (width)
+		{
+			--(*ind);
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+				--(*ind);
+			if (fmt[*ind] == ' ')
+				--(*ind);
+			return (1);
+		}
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
 	}
-
-	else
-
-		num = n;
-
-
-
-	for (; num / check > 9; )
-
-		check *= 10;
-
-
-
-	for (; check != 0; )
-
-	{
-
-		len += _putchar('0' + num / check);
-
-		num %= check;
-
-		check /= 10;
-
-	}
-
-
-
-	return (len);
-
+	return (printed_chars);
 }
 
-
-
-int print_unsgined_number(unsigned int n)
-
-{
-
-	int check;
-
-	int len;
-
-	unsigned int num;
-
-
-
-	check = 1;
-
-	len = 0;
-
-
-
-	num = n;
-
-
-
-	for (; num / check > 9; )
-
-		check *= 10;
-
-
-
-	for (; check != 0; )
-
-	{
-
-		len += _putchar('0' + num / check);
-
-		num %= check;
-
-		check /= 10;
-
-	}
-
-
-
-	return (len);
-
-}
-
-
-
-
-
-
-
-
-
-
-
-int p_char(va_list args)
-
-{
-
-	char value;
-
-
-
-	value = va_arg(args, int);
-
-	_putchar(value);
-
-	return (1);
-
-}
-
-
-
-
-
-
-
-int p_string(va_list args)
-
-{
-
-	int i;
-
-	const char *s;
-
-
-
-	s = va_arg(args, const char *);
-
-	if (s == NULL)
-
-		s = "(null)";
-
-	for (i = 0; s[i] != '\0'; i++)
-
-		_putchar(s[i]);
-
-	return (i);
-
-}
-
-
-
-
-
-
-
-int p_percent(__attribute__((unused)) va_list args)
-
-{
-
-	_putchar('%');
-
-	return (1);
-
-}
-
-
-
-
-
-
-
-int p_integer(va_list args)
-
-{
-
-	int n;
-
-
-
-	n = print_number(args);
-
-	return (n);
-
-
-
-}
